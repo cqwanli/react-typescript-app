a672dd4ed074c5bf332beff6915d14d1326a9842
wanli <wanli@iot.chinamobile.com>
2018-07-13 14:10:53 +0800

    添加editorConfig

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import { Route } from 'react-router-dom'
 import routes from '../routes';
 class App extends React.Component {
-    public render() {
-        return (
-            <div>
-                {
-                    routes.map((route, i) => (
-                        <Route path={route.path} key={i} component={route.component} />))
-                }
-            </div>
-        );
-    }
+  public render() {
+    return (
+      <div>
+        {
+          routes.map((route, i) => (
+            <Route path={route.path} key={i} component={route.component} />))
+        }
+      </div>
+    );
+  }
 }
 export default App;
